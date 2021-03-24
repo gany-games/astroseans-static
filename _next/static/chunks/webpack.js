4aafc6eacb5f043a1ee45e734ed07a0911869f55
muratGG <murat.atay@ganygames.com>
2021-03-24 23:47:07 +0300

    Mobile design changes.

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f9ac2acf33b55dd10728";
+/******/ 	var hotCurrentHash = "7eb1bad3f4120bce0106";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
